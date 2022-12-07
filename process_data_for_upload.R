140b179610fad4ce05f868f62439499538ce4252
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2022-12-07 07:48:33 -0500

    Update process_data_for_upload.R

--- process_data_for_upload.R
+++ process_data_for_upload.R
@@ -1,246 +1,119 @@
-#!/usr/bin/env Rscript --vanilla
+# This script uses some simple code to de-identify Seurat object and add metadata (like cell annotations). The required inputs are - Seurat obj, table with sample/orig.ident (e.g. patientID1,patientID2,etc.) and the ID to change it to (e.g. U1,U2,etc.) in columns named 'SampleID' and 'Updated_ID', respectively. Addiitionally a metadata spreadsheet is needed that contains seurat cluster numbers and their matching annotations.
 
-#Loading dependencies for working in command line and HPC environment
-library(argparser)
-library(parallel) 
-library(tictoc)
+# Author: Surbhi Sona (Ting Lab)
 
-#Loading dependencies for data wrangling and processing
-library(dplyr) 
-library(data.table) 
-
-#'omics' packages
 library(Seurat)
-library(Signac)
-library(GenomicRanges)
-library(patchwork)
-library(microbenchmark)
+library(dplyr)
+library(data.table)
+library(openxlsx)
+library(readxl)
+library(tidyverse)
 
-#Visualizations packages
-library(ggplot2)
-library(lattice)    
-library(grid) 
-library(gridExtra)
-
-source('/bme/home/surbhis/Scripts/R_Scripts_SS/sc_atac_functions.R')
-##### Creating options to add command line arguments #####
-
-parser<-arg_parser(name="sc_atac_merge.R",description="Processing multiple atac seq data using Seurat pipeline")
-
-parser<-add_argument(
-  parser,
-  arg="--input_dir",
-  short = '-i',
-  type="character",
-  default="./",
-  help="Location+name of directory containing matrix count data.Must have trailing '/'. Default: [./]"
-)
-
-parser<-add_argument(
-  parser,
-  arg="--output_dir",
-  short = '-o',
-  type="character",
-  default="./data/cluster_plots/",
-  help="location+name for output directory. Must contain trailing '/'. Default: [./data/cluster_plots/]"
-)
-
-parser<-add_argument(
-  parser,
-  arg="--samples",
-  short = '-s',
-  type="character",
-  default="",
-  help="Name of sample to be processed. Must be name of sample directory. For multiple samples, split by comma (,)"
-)
-
-parser<-add_argument(
-  parser,
-  arg="--run_name",
-  short = '-r',
-  type="character",
-  default="",
-  help="Title used to identify all plots that correspond to the current run instance. Default: None"
-)
-
-parser<-add_argument(
-  parser,
-  arg="--matrix_name",
-  short = '-n',
-  type="character",
-  default="filtered_peak_bc_matrix",
-  help="Name of the matrix file/directory with no file extension. Eg: 'raw_feature_bc_matrix'. Default: [filtered_feature_bc_matrix]"
-)
-
-parser<-add_argument(
-  parser,
-  arg="--annotation_file",
-  short = '-a',
-  type="character",
-  default="/home/surbhis/ref_files_bc/Homo_sapiens.GRCh37.87.gtf",
-  help="Enter the annotation file with path. Default:[gtf file in cellranger ref folder]"
-)
-
-parser<-add_argument(
-  parser,
-  arg="--meta_file",
-  short = '-m',
-  type="character",
-  default="",
-  help="Enter the meta data file in csv format along with path"
-)
-
-parser<-add_argument(
-  parser,
-  arg="--rna_file",
-  short = '-f',
-  type="character",
-  default="",
-  help="Enter the processed sc-RNA seq file in rds format along with path"
-)
-
-parser<-add_argument(
-  parser,
-  arg="--cores",
-  short = '-c',
-  type="character",
-  default='1',
-  help= "Number of cores to use when processing samples in parallel. Use 'all' to use all available cores. Windows MUST use 1. Default: 1"
-)
-
-parser<-add_argument(
-  parser,
-  arg="--verbose",
-  short = '-v',
-  flag=TRUE,
-  help="Flag set for verbose output. Warning: May lead to performance drops."
-)
-
-argv <- parse_args(parser)
-
-argv
-
-##### Argument Processing #####
-
-#Determine number of cores
-if(length(grep('^all$',argv$cores,ignore.case = TRUE)) != 0){
-  argv$cores<-detectCores()
-}else{
-  argv$cores<-as.numeric(argv$cores)
-}
-
-#Validate sample names
-if(argv$samples==''){
-  if(argv$verbose){
-    cat("Sample is empty. Terminating Run",sep='\n')
-    tic.clear()
-    quit(save = 'no')
-  }
-  quit()
-}else
-  {samples=unlist(strsplit(argv$samples, split=','))}
-
-# Prepare individual filenames using run ID
-run_file_name<-''
-if(argv$run_name!=''){
-  run_file_name<- gsub(' ','_',argv$run_name)
-  run_file_name<-paste0(run_file_name,'-')
-}
+# Read original Seurat obj
+obj_g<-readRDS('/home/sonas/tingalab/Manuscripts/2021_UreterManuscript/ANALYSIS/Seurat/2020_09_21_ureter10/2020_09_21_ureter10_clustered.rds')
+obj_str<-readRDS('/home/sonas/tingalab/Manuscripts/2021_UreterManuscript/ANALYSIS/Seurat/Stromal subset/2021_02_19_ureter10_stromal/2021_02_19_ureter10_stromal_clustered.rds')
+obj_uro<-readRDS('/home/sonas/tingalab/Manuscripts/2021_UreterManuscript/ANALYSIS/Seurat/Uro subset/2021_04_01_ureter10_uro_PC50_res0.2/2021_04_01_ureter10_uro_PC50_res0.2_clustered.rds')
+obj_imm<-readRDS('/home/sonas/tingalab/Manuscripts/2021_UreterManuscript/ANALYSIS/Seurat/Immune subset/2021_02_25_ureter10_immune/2021_02_25_ureter10_immune_clustered.rds')
+obj_trio<-readRDS('/home/sonas/tingalab/Manuscripts/2021_UreterManuscript/ANALYSIS/Seurat/2020_09_22_ureter3_PC30_res0.3/2020_09_22_ureter3_PC30_res0.3_clustered.rds')
 
 
-#Delete Later !!!
-add_function_or_delete<-funtion()
+# Set variables
+file<-'/home/sonas/tingalab/Manuscripts/2021_UreterManuscript/SampleID_Key.xlsx'
+sample_tab<-read_xlsx(file)
+
+obj_out<-'/home/sonas/ucsc/'
+
+# New filenames
+prefix_global<-'adult-ureter'
+prefix_uro<-'adult-ureter-uro-subset'
+prefix_str<-'adult-ureter-stromal-subset'
+prefix_imm<-'adult-ureter-immune-subset'
+prefix_trio<-'adult-ureter-trio'
+
+
+# Function to de-identify Seurat object
+
+de_identify_obj<-function(obj,sample_tab,out,fname,save=TRUE)
 {
-    
-     #Data pre-processing
-    DefaultAssay(sample.atac)<-"ACTIVITY"
-    sample.atac<-FindVariableFeatures(sample.atac)
-    sample.atac<-NormalizeData(sample.atac)
-    sample.atac<-ScaleData(sample.atac)
-
-     #Process peak matrix
-    DefaultAssay(sample.atac)<-"peaks"
-    VariableFeatures(sample.atac)<-names(which(Matrix::rowSums(sample.atac)>100))
-    sample.atac<-RunLSI(sample.atac, n=50, scale.max=NULL)
-    sample.atac<-RunUMAP(sample.atac,reduction="lsi",dims=1:50)
-
-    #sample.atac$dataset<-sample_name
-
-    return(sample.atac)
-sample.atac.filtered<-subset(sample.atac, subset=prediction.score.max>0.5)
-sample.atac.filtered$predicted.id<-factor(sample.atac.filtered$predicted.id, levels=levels(sample.rna))
-
-sample<-list(sample.atac,sample.atac.filtered)
-return(sample)
-
-#Filter
-sample.atac.filtered<-subset(sample.atac, subset=prediction.score.max>0.5)
-sample.atac.filtered$predicted.id<-factor(sample.atac.filtered$predicted.id, levels=levels(sample.rna))
-
-sample<-list(sample.atac,sample.atac.filtered)
-return(sample)
-}
-
+  indx<-match(obj$orig.ident,sample_tab$Sample)
+  obj$orig.ident<-sample_tab$Updated_ID[indx]
+  indx2<-match(obj$sample,sample_tab$Sample)
+  obj$sample<-sample_tab$Updated_ID[indx2]
+  if(save)
+  {
+    saveRDS(obj,file=paste0(out,fname))
+  }else{
+    return(obj)
+  }
 }
 
 
-##### Main Script #####
+# function to add cell annotations to Seurat obj metadata
 
-## Create atac seq Seurat object ##
-n<-length(samples)
-atac.obj.list<-list()
+# Update Metadata
 
-atac.obj.list<-lapply(samples[1:n],create_atac_obj,argv$input_dir,argv$annotation_file)
-
-## Sample QC Plots ##
-lapply(atac.obj.list[1:n],get_qc_plots,argv$out_dir)
-#atac.obj.list<-lapply(samples[1:n],get_tss_enrichment, samples)
-
-
-## Merge data ##
-# Create common peak set
-combined.peaks<-UnifyPeaks(object.list=atac.obj.list, mode="reduce")
-
-# Quantify peaks in each set
-atac.obj.list<-lapply(atac.obj.list, quantify_peaks, combined.peaks)
-
-# Merge sc.atac object
-fragment.path<-dirname(argv$input_dir)
-fragment.path<-paste0(fragment.path,'/fragments.tsv.gz')
-atac.merged<-merge_atac_data(atac.obj.list,fragment.path,argv$samples)
-atac.merged$tech<-'atac'
-
-## Filter cells ##
-get_qc_plots(atac.merged,out_dir=argv$out_dir,sample.name='merged')
-
-atac.merged<-filter_cells(atac.merged, peak_frag=thres[1:2],read_pct=thres[3],blk_ratio=thres[4], nuc_signal=thres[5], tss_enrich=thres[6])
-
-## Data pre-process and clustering ##
-atac.merged<-data_pre_process_activity(atac.merged)
-atac.merged<-data_pre_process_atac(atac.merged)
-atac.merged<-get_clusters(atac.merged)
-
-print_umap(atac.merged, plotTitle=argv$run_name,feature=NULL,oDir=argv$output_dir,PrintLabels=TRUE)
-
-## Add RNA seq data ##
-
-#Read RNA seq file
-sample.rna<-readRDS(argv$rna_file)
-sample.rna$tech<-"rna"
-
-print_umap(sample.rna, feature=NULL,titleGroup='sc-RNA-seq',plotTitle="RNAseq", PrintLabels=TRUE)
-
-#integrate RNA data
+update_metacol<-function(obj,tab_path,tab_name,out,fprefix,save=TRUE)
+{
+  #Remove redundant seurat cluster column
+  col<-grep('integrated',colnames(obj@meta.data))
+  obj@meta.data[,col]<-NULL
+  
+  # Read table
+  tab<-read_xlsx(paste0(tab_path,tab_name))
+  
+  # Add cell type info
+  indx<- match(obj$seurat_clusters,tab$clusters)
+  cell_type<-tab$cell_type[indx]
+  obj<-AddMetaData(obj,metadata = cell_type,col.name = 'cell_type')
+  
+  if(save==TRUE)
+  {
+    saveRDS(obj,paste0(out,fprefix,'.rds'))
+  }else
+  {
+    return(obj)
+  }
+  
+  
+}
 
 
-atac.merged<-integrate_rna_atac(atac.merged, sample.rna)
 
-#co-embed RNA and atac data
-coembed<-run_coembed_process(sample.atac, sample.rna)
+## Deidentify 
 
-print_umap(coembed, feature="tech")
-print_umap(coembed, feature="celltype", PrintLabels=TRUE,set_repel=TRUE)
-print_umap(coembed, split="tech", feature="celltype",Printlabels=TRUE,set_repel=TRUE)
+de_identify_obj(obj=obj_g,sample_tab = sample_tab,out=obj_out,fname=paste0(prefix_global,'.rds'),save = TRUE)
+de_identify_obj(obj=obj_uro,sample_tab = sample_tab,obj_out,fname=paste0(prefix_uro,'.rds'),save = TRUE)
+de_identify_obj(obj=obj_str,sample_tab = sample_tab,obj_out,fname=paste0(prefix_str,'.rds'),save = TRUE)
+de_identify_obj(obj=obj_imm,sample_tab = sample_tab,obj_out,fname=paste0(prefix_imm,'.rds'),save = TRUE)
+de_identify_obj(obj=obj_trio,sample_tab = sample_tab,obj_out,fname=paste0(prefix_trio,'.rds'),save = TRUE)
 
-cat("End of Script", '\n')
+
+
+## Add metadata
+path<-'/home/sonas/tingalab/Manuscripts/2021_UreterManuscript/ANALYSIS/UCSC/cluster_annotation_tables/'
+
+# These files should contain 2 columns: 
+#'clusters' that denote seurat cluster (e.g. 0,1,2) and 'cell_type' which contains matching cell annotations for each cluster (e.g. basal cells, umbrella cells, intetermidiate cells)
+
+global<-'global.xlsx'
+immune<-'immune.xlsx'
+str<-'stromal.xlsx'
+uro<-'uro.xlsx'
+trio<-'trio.xlsx'
+
+# Read in de-identified obj
+
+obj_g2<-readRDS(paste0(obj_out,prefix_global,'.rds'))
+obj_uro2<-readRDS(paste0(obj_out,prefix_uro,'.rds'))
+obj_str2<-readRDS(paste0(obj_out,prefix_str,'.rds'))
+obj_imm2<-readRDS(paste0(obj_out,prefix_imm,'.rds'))
+obj_trio2<-readRDS(paste0(obj_out, prefix_trio,'.rds'))
+
+# add cell annotation
+update_metacol(obj=obj_g2,tab_path = path,tab_name=global,out=obj_out,fprefix = prefix_global, save=TRUE)
+update_metacol(obj=obj_str2,tab_path = path,tab_name=str,out=obj_out,fprefix = prefix_str, save=TRUE)
+update_metacol(obj=obj_imm2,tab_path = path,tab_name=immune,out=obj_out,fprefix = prefix_imm, save=TRUE)
+update_metacol(obj = obj_uro2,tab_path = path,tab_name=uro,out=obj_out,fprefix = prefix_uro, save=TRUE)
+update_metacol(obj = obj_trio2,tab_path = path,tab_name=trio,out=obj_out,fprefix = prefix_trio, save=TRUE)
+
 
